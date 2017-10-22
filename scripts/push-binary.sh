a4a08e4cf8f186f792e54e5c14febf21356ca8f1
Travis CI <travis@travis-ci.org>
2017-10-23 00:21:41 +0200

    remove travis log check

--- scripts/push-binary.sh
+++ scripts/push-binary.sh
@@ -3,7 +3,7 @@ git config --global user.name "Travis CI"
 
 git add -A
 git remote rm origin
-git remote add origin https://gidsi:foobar@github.com/gidsi/spaceapi-validator-cli
+git remote add origin https://gidsi:$GITHUB_API_KEY@github.com/gidsi/spaceapi-validator-cli
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci]"
-git push origin master
+git push origin master > /dev/null
 echo -e "Done\n"
