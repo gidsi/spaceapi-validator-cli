08d978b1832106307fe203d00f4cc9b4bda607a3
Timm <gidsi@chaospott.de>
2017-10-22 23:57:46 +0200

    some push script refactoring

--- scripts/push-binary.sh
+++ scripts/push-binary.sh
@@ -1,9 +1,9 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git add -f .
+git add -A
 git remote rm origin
 git remote add origin https://gidsi:$GITHUB_API_KEY@github.com/gidsi/spaceapi-validator-cli
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
-git push -fq origin master > /dev/null
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci]"
+git push origin master > /dev/null
 echo -e "Done\n"
