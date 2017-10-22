de2dbdd05440c0827f3723c8207226714be10975
gidsi <gidsi@chaospott.de>
2017-10-22 23:27:36 +0200

    travis build test

---
+++ scripts/push-binary.sh
@@ -0,0 +1,12 @@
+cd $HOME
+git config --global user.email "gidsi@chaospott.de"
+git config --global user.name "Timm"
+
+git clone --quiet --branch=master  https://gidsi:$GITHUB_API_KEY@github.com/gidsi/spaceapi-validator-cli  master > /dev/null
+
+git add -f .
+git remote rm origin
+git remote add origin https://gidsi:$GITHUB_API_KEY@github.com/gidsi/spaceapi-validator-cli
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
+git push -fq origin master > /dev/null
+echo -e "Done\n"
