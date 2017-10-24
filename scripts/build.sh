2d6dc887988f2da4a69db18126c77cd1f3f169e2
gidsi <gidsi@chaospott.de>
2017-10-24 02:53:16 +0200

    travis test for multi arch binaries

---
+++ scripts/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+mkdir bin
+
+for R in       \
+    darwin/386     \
+    darwin/amd64   \
+    freebsd/386    \
+    freebsd/amd64  \
+    freebsd/arm    \
+    linux/386      \
+    linux/arm64    \
+    linux/arm      \
+    linux/arm64    \
+    openbsd/386   \
+    openbsd/amd64 \
+    windows/386    \
+    windows/amd64  \
+    ; do \
+
+    os=$(dirname ${R})
+    arch=$(basename ${R})
+    filename=spaceapi-validator-${os}-${arch}
+
+    if [[ "$os" == "windows" ]]; then
+        filename="${filename}.exe"
+    fi
+
+    echo ${filename}
+
+    GOOS=${os} GOARCH=${arch} go build -o "bin/${filename}"
+
+done
+
+cd "bin"
+sha256sum spaceapi-validator-* > SHA256SUMS
+
+echo "done"
